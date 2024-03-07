ae71997bd403334814252f59ab2afe6e5997acbf
Erivaldo Jr <eaojunior1@gmail.com>
2024-03-06 21:16:32 -0300

    Adiciona CIDRs nas variaveis

--- .terraform/infra-estructure/variables.tf
+++ .terraform/infra-estructure/variables.tf
@@ -1,3 +1,13 @@
+variable "cidrs" {
+  type        = map(string)
+  description = "Bloco de endereços IP do projeto"
+  default = {
+    develop = "172.20.0.0/16"
+    homologation = "172.22.0.0/16"
+    main = "172.16.0.0/16"
+  }
+}
+
 variable "env" {
   description = "The environment in which resources are set up"
   type        = string
